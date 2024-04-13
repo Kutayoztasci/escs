d95d9c0d9dbaf24db8f768163a151a54823a1aa6
Kutayoztasci <kutayoztasci96@gmail.com>
2024-04-13 17:21:16 +0300

    Update matchController.js

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -41,6 +41,16 @@ router.get('/matchDetail/:id', async (req, res) => {
     }
 });
 
+router.get('/matchById/:id', async (req, res) => {
+    try {
+        const endpoint = `/matches/${req.params.id}`;
+        const data = await fetchDataFromApi(endpoint, req);
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 router.get('/liveMatches/:game', async (req, res) => {
     try {
         const gameId = gameData.validateGameId(req.params.game, res);
@@ -77,7 +87,12 @@ router.get('/oldMatches/:game/:id', async (req, res) => {
 
         const endpoint = `/teams/${req.params.id}/series${gameId}&filter=lifecycle=over`;
         const data = await fetchDataFromApi(endpoint, req);
-
+        /*var take;
+        if(req.query.take !== undefined && req.query.take !== null){
+            take = req.query.take;
+        }else{
+            take = 50;
+        }*/
         var matchesList = await collectMatches(data, req);
 
         res.json(matchesList);
@@ -92,29 +107,11 @@ router.get('/playerMatches/:id', async (req, res) => {
         const endpoint = `/players/${req.params.id}/standingrosters`;
         const rosters = await fetchDataFromApi(endpoint, req);
         var matchesList = await matchesWithRosters(rosters, req);
-
-        /*matchesList.sort(function(a,b) {
-            return new Date(a.created_at) - new Date(b.created_at);
-        });
-
-        matchesList.forEach(function(abcdf){
-            console.log(abcdf);
-        });
-        */
         res.json(matchesList);
     } catch (error) {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
-
-function dateSort(a, b){
-    if (!a.created_at || !b.created_at) {
-        return 0;
-    }
-    var a = Date.parse(a.created_at.substring(0,23) + a.created_at.substring(26));
-    var b = Date.parse(b.created_at.substring(0,23) + b.created_at.substring(26));
-    return new Date(b).getTime() - new Date(a).getTime();
-    }
 
 async function collectMatches(data, req) {
     var idList = [];
@@ -126,17 +123,29 @@ async function collectMatches(data, req) {
         });
     });
 
-    for (const id of idList) {
-        var match = await getMatchesById(id, req);
-        responseDataList.push(match);
+    const chunkedIds = chunkArray(idList, 20);
+    for (const chunk of chunkedIds) {
+        const idString = chunk.join(',');
+        const matches = await getMatchesById(idString, req);
+        if (matches !== undefined && matches !== null && matches.length !== 0) {
+            responseDataList.push(matches);
+        }
     }
+
+    /*for (const id of idList) {
+        var match = await getMatchesById(id, req);
+        if (match !== undefined && match !== null && match.length !== 0){
+            responseDataList.push(match);
+
+        }
+    }*/
 
     return responseDataList;
 }
 
 async function getMatchesById(id, req) {
     try {
-        endpoint = `/matches/${id}`;
+        endpoint = `/matches?filter=id<={${id}}`;
         const response = await fetchDataFromApi(endpoint, req);
         return response;
     } catch (error) {
@@ -166,6 +175,14 @@ async function getMatchesByRoster(id, req) {
     } catch (error) {
         console.error(`${error}`);
     }
+}
+
+function chunkArray(array, chunkSize) {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        chunks.push(array.slice(i, i + chunkSize));
+    }
+    return chunks;
 }
 
 module.exports = router;
